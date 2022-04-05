f535cb63580bfc8ae98e806e4085433dec54a5b6
JiayingShentu <2386721963@qq.com>
2022-04-06 00:52:09 +0800

    item-left-done

--- week4/simple-react/src/components/footer.jsx
+++ week4/simple-react/src/components/footer.jsx
@@ -3,27 +3,25 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
     const { todos } = this.props;
-
+    const total = todos.filter(todo => todo.completed == false).length;
     return (
       <div className="footer">
         <span className="todo-count">
-          <strong>X</strong>
+          <strong>{total}</strong>
           <span> items</span>
           <span> left</span>
         </span>
         <ul className="filters">
           <li>
-            <a href="#" className="selected">
-              All
-            </a>
+            <a className="selected">All</a>
           </li>
           <span></span>
           <li>
-            <a href="#">Active</a>
+            <a>Active</a>
           </li>
           <span></span>
           <li>
-            <a href="#">Completed</a>
+            <a>Completed</a>
           </li>
         </ul>
       </div>
